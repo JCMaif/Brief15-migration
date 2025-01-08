de75b59ee456bc362fd3f1a0eab08534e78969cd
Jean-Charles Audebert <jean-charles.audebert@maif.fr>
2025-01-08 09:40:07 +0100

    first commit

---
+++ src/test/java/com/simplon/Brief15/Brief15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplon.Brief15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Brief15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
